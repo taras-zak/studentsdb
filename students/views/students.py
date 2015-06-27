900fd3b215a73f06589fff22d7ec0afd73ca8d03
TarasZak <taras.zakan@gmail.com>
2015-06-27 20:06:07 +0300

    add editing of student

--- students/views/students.py
+++ students/views/students.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.urlresolvers import reverse
+
 from django.contrib import messages
 from django.contrib.messages import get_messages
-from datetime import datetime
+
+from django.forms import ModelForm
+from django.views.generic import UpdateView
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from crispy_forms.bootstrap import FormActions
+
 from ..models import Student, Group
 
 # Students
@@ -130,8 +140,49 @@ def students_add(request):
 	return render(request, 'students/students_add.html',
 		{'groups': Group.objects.all().order_by('title')})
 
-def students_edit(request, sid):
-	return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+class StudentUpdateForm(ModelForm):
+	class Meta:
+		model = Student
+
+	def __init__(self, *args, **kwargs):
+		super(StudentUpdateForm, self).__init__(*args, **kwargs)
+
+		self.helper = FormHelper(self)
+
+		# set form tag attributes
+		self.helper.form_action = reverse('students_edit',kwargs={'pk': kwargs['instance'].id})
+		self.helper.form_method = 'POST'
+		self.helper.form_class = 'form-horizontal'
+
+		# set form field properties
+		self.helper.help_text_inline = True
+		self.helper.html5_required = True
+		self.helper.label_class = 'col-sm-2 control-label'
+		self.helper.field_class = 'col-sm-4'
+
+		# add buttons
+		self.helper.layout[-1] = FormActions(
+			Submit('add_button', u'Зберегти', css_class="btn btn-primary"),
+			Submit('cancel_button', u'Скасувати', css_class="btn btn-link"),
+		)
+
+class StudentUpdateView(UpdateView):
+	model = Student
+	template_name = 'students/students_edit.html'
+	form_class = StudentUpdateForm
+
+	def get_success_url(self):
+		return u'%s?status_message=Студента успішно збережено!'	% reverse('home')
+
+	def post(self, request, *args, **kwargs):
+		if request.POST.get('cancel_button'):
+			return HttpResponseRedirect(
+			u'%s?status_message=Редагування студента відмінено!' %
+		reverse('home'))
+		else:
+			return super(StudentUpdateView, self).post(request, *args, **kwargs)
+
+
 
 def students_delete(request, sid):
 	return HttpResponse('<h1>Delete Student %s</h1>' % sid)
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from .settings import MEDIA_ROOT, DEBUG
+from students.views.students import StudentUpdateView
 
 urlpatterns = patterns('',
     # Students urls
 
     url(r'^$', 'students.views.students.students_list', name='home'),
 	url(r'^students/add/$', 'students.views.students.students_add',name='students_add'),
-	url(r'^students/(?P<sid>\d+)/edit/$','students.views.students.students_edit',name='students_edit'),
+	url(r'^students/(?P<pk>\d+)/edit/$',StudentUpdateView.as_view(),name='students_edit'),
 	url(r'^students/(?P<sid>\d+)/delete/$','students.views.students.students_delete',name='students_delete'),
 	
 	# Groups urls
