0c82bd54400a0f2930f43337c1d133ad6c13883c
taras-zak <taras.zakan@gmail.com>
2015-08-23 00:32:16 +0300

    add registration

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -87,11 +87,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_CONTEXT_PROCESSORS = \
-	global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
-	"django.core.context_processors.request",
-	'django.contrib.messages.context_processors.messages',
-	"studentsdb.context_processors.students_proc",
-	"students.context_processors.groups_processor",
+    global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
+        "django.core.context_processors.request",
+        "django.contrib.messages.context_processors.messages",
+        "studentsdb.context_processors.students_proc",
+        "students.context_processors.groups_processor",
     )
 
 TEMPLATE_DIRS = (
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -10,42 +10,42 @@ urlpatterns = patterns('',
     # Students urls
 
     url(r'^$', 'students.views.students.students_list', name='home'),
-	url(r'^students/add/$', 'students.views.students.students_add',name='students_add'),
-	url(r'^students/(?P<pk>\d+)/edit/$',StudentUpdateView.as_view(),name='students_edit'),
-	url(r'^students/(?P<pk>\d+)/delete/$',StudentDeleteView.as_view(),name='students_delete'),
-	
-	# Groups urls
+    url(r'^students/add/$', 'students.views.students.students_add', name='students_add'),
+    url(r'^students/(?P<pk>\d+)/edit/$', StudentUpdateView.as_view(), name='students_edit'),
+    url(r'^students/(?P<pk>\d+)/delete/$', StudentDeleteView.as_view(), name='students_delete'),
 
-	url(r'^groups/$', 'students.views.groups.groups_list', name='groups'),
-    url(r'^groups/add/$', 'students.views.groups.groups_add',name='groups_add'),
-	url(r'^groups/(?P<gid>\d+)/edit/$','students.views.groups.groups_edit',name='groups_edit'),
-	url(r'^groups/(?P<gid>\d+)/delete/$','students.views.groups.groups_delete',name='groups_delete'),
+    # Groups urls
 
-	# Journal urls
+    url(r'^groups/$', 'students.views.groups.groups_list', name='groups'),
+    url(r'^groups/add/$', 'students.views.groups.groups_add', name='groups_add'),
+    url(r'^groups/(?P<gid>\d+)/edit/$', 'students.views.groups.groups_edit', name='groups_edit'),
+    url(r'^groups/(?P<gid>\d+)/delete/$', 'students.views.groups.groups_delete', name='groups_delete'),
 
-	url(r'^journal/$', JournalView.as_view(), name='journal'),
+    # Journal urls
 
-	# Exams urls
+    url(r'^journal/$', JournalView.as_view(), name='journal'),
 
-	url(r'^exams/$', 'students.views.exams.exams_list', name='exams'),
-	url(r'^exams/(?P<eid>\d+)/edit/$','students.views.exams.exam_edit',name='exam_edit'),
-	url(r'^exams/(?P<eid>\d+)/delete/$','students.views.exams.exam_delete',name='exam_delete'),
+    # Exams urls
 
-	# Contact urls
-	url(r'^contact-admin/$', 'students.views.contact_admin.contact_admin', name='contact_admin'),
+    url(r'^exams/$', 'students.views.exams.exams_list', name='exams'),
+    url(r'^exams/(?P<eid>\d+)/edit/$', 'students.views.exams.exam_edit', name='exam_edit'),
+    url(r'^exams/(?P<eid>\d+)/delete/$', 'students.views.exams.exam_delete', name='exam_delete'),
 
-	#admin
+    # Contact urls
+    url(r'^contact-admin/$', 'students.views.contact_admin.contact_admin', name='contact_admin'),
+
+    #admin
     url(r'^admin/', include(admin.site.urls)),
 
     # User Related urls
-	url(r'^users/logout/$', auth_views.logout, kwargs={'next_page':	'home'}, name='auth_logout'),
-	url(r'^register/complete/$', RedirectView.as_view(pattern_name='home'),	name='registration_complete'),
-	url(r'^users/', include('registration.backends.simple.urls',namespace='users')),
+    url(r'^users/logout/$', auth_views.logout, kwargs={'next_page':	'home'}, name='auth_logout'),
+    url(r'^register/complete/$', RedirectView.as_view(pattern_name='home'),	name='registration_complete'),
+    url(r'^users/', include('registration.backends.simple.urls', namespace='users')),
 
 )
 
 if DEBUG:
-	# serve files from media folder
-		urlpatterns += patterns('',
-			url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-				'document_root': MEDIA_ROOT}))
+    # serve files from media folder
+        urlpatterns += patterns('',
+                                url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+                                    'document_root': MEDIA_ROOT}))
