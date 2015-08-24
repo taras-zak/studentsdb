8dcf31d1f124b97d6b65e49387e1aa6746362572
taras-zak <taras.zakan@gmail.com>
2015-08-24 21:12:26 +0300

    expanded user profile

--- stud_auth/models.py
+++ stud_auth/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class StProfile(models.Model):
+    """ To keep extra user data"""
+
+    #user mapping
+    user = models.OneToOneField(User)
+
+    class Meta(object):
+        verbose_name = u"User Profile"
+
+    #extra user data
+    mobile_phone = models.CharField(
+        max_length=12,
+        blank=True,
+        verbose_name=u"Mobile Phone",
+        default='')
+
+    def __unicide__(self):
+        return self.user.username
