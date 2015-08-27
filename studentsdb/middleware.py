94276e980999079058212c67746bbcd030c05d64
taras-zak <taras.zakan@gmail.com>
2015-08-28 00:56:10 +0300

    try add "Request time" to <body>

--- studentsdb/middleware.py
+++ studentsdb/middleware.py
@@ -1,13 +1,14 @@
 from datetime import datetime
 from django.http import HttpResponse
-from studentsdb import settings
+from studentsdb.settings import DEBUG
+from bs4 import BeautifulSoup
 
 
 class RequestTimeMiddleware(object):
     """Display request time on a page"""
 
     def process_request(self, request):
-        if settings.DEBUG == True:
+        if DEBUG == True:
             request.start_time = datetime.now()
         return None
 
@@ -19,9 +20,22 @@ class RequestTimeMiddleware(object):
 
         # calculate request execution time
         request.end_time = datetime.now()
-        if 'text/html' in response.get('Content-Type', ''):
-            response.write('<br/>Request took: %s' % str(request.end_time - request.start_time))
 
+        if 'text/html' in response.get('Content-Type', ''):
+            response.soup = BeautifulSoup(response.content)
+            response.original_tag = response.soup.body
+            response.new_tag = response.soup.new_tag("div")
+            response.new_tag.string = 'Request took: %s' % str(request.end_time - request.start_time)
+            response.original_tag.append(response.new_tag)
+            #response.content = response.soup
+            #soup.body.append('Request took: %s' % str(request.end_time - request.start_time))
+        print(response.content)
+        
+        
+        #if 'text/html' in response.get('Content-Type', ''):
+        #    response.write('<br/>Request took: %s' % str(request.end_time - request.start_time))
+        #print type(response.content)
+        #print request
         return response
 
     def process_view(self, request, view, args, kwargs):
