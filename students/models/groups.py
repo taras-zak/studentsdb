87733bb3e02c949cb8de2fcd5f64e2073f4124da
taras-zak <taras.zakan@gmail.com>
2015-09-21 23:21:46 +0300

    group add form and validation

--- students/models/groups.py
+++ students/models/groups.py
@@ -3,30 +3,30 @@
 from django.db import models
 
 class Group(models.Model):
-	"""Group Model"""
+    """Group Model"""
 
-	class Meta(object):
-		verbose_name = u"Група"
-		verbose_name_plural = u"Групи"
+    class Meta(object):
+        verbose_name = u"Група"
+        verbose_name_plural = u"Групи"
 
-	title = models.CharField(
-		max_length=256,
-		blank=False,
-		verbose_name=u"Назва")
+    title = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name=u"Назва")
 
-	leader = models.OneToOneField('Student',
-		verbose_name=u"Староста",
-		blank=True,
-		null=True,
-		on_delete=models.SET_NULL)
+    leader = models.OneToOneField('Student',
+        verbose_name=u"Староста",
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL)
 
-	notes = models.TextField(
-		blank=True,
-		verbose_name=u"Додаткові нотатки")
+    notes = models.TextField(
+        blank=True,
+        verbose_name=u"Додаткові нотатки")
 
-	def __unicode__(self):
-		if self.leader:
-			return u"%s (%s %s)" % (self.title, self.leader.first_name,
-		self.leader.last_name)
-		else:
-			return u"%s" % (self.title,)
+    def __unicode__(self):
+        if self.leader:
+            return u"%s (%s %s)" % (self.title, self.leader.first_name,
+        self.leader.last_name)
+        else:
+            return u"%s" % (self.title,)
--- students/views/groups.py
+++ students/views/groups.py
@@ -9,12 +9,15 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
 from django.contrib import messages
+from django.contrib.messages import get_messages
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from django.db.models.deletion import ProtectedError
+from django.db import IntegrityError
 
-from ..models import Group
+from ..models import Group, Student
+from ..util import paginate, get_current_group
 
 #Groups
 def groups_list(request):
@@ -46,7 +49,57 @@ def groups_list(request):
 
 
 def groups_add(request):
-    return HttpResponse('<h1>Group Add Form</h1>')
+    if request.method == "POST":
+        # was form add button clicked?
+        if request.POST.get('add_button') is not None:
+            
+            # data for group object
+            data = {'notes': request.POST.get('notes')}
+
+            # validate user input
+            title = request.POST.get('title', '').strip()
+            if not title:
+                messages.add_message(request, messages.WARNING, "Назва групи є обов'язковою")
+            else:
+                data['title'] = title
+
+            leader = request.POST.get('leader', '').strip()
+            if leader:
+                students = Student.objects.filter(pk=leader)
+                if hasattr(students[0], 'group'):
+                    messages.add_message(request, messages.WARNING, "Обраний студент вже є старостою")
+                if len(students) != 1:
+                    #errors['student_group'] = u"Оберіть коректну групу"
+                    messages.add_message(request, messages.WARNING, "Оберіть коректну групу")
+                else:
+                    data['leader'] = students[0]
+
+            # save student
+            storage = get_messages(request)
+            if not storage:
+                # create student object
+                group = Group(**data)
+                group.save()
+                # redirect user to students list
+                messages.add_message(request, messages.SUCCESS, 'Групу %s успішно додано!'% group)
+                return HttpResponseRedirect(reverse('groups'))
+            else:
+                # render form with errors and previous user input
+                return render(request, 'students/groups_add.html',
+                    {'students': Student.objects.all().order_by('last_name')})
+
+        elif request.POST.get('cancel_button') is not None:
+            # redirect to home page on cancel button
+            messages.add_message(request, messages.INFO, 'Додавання групи скасовано!')
+            return HttpResponseRedirect(reverse('groups'))
+    else:
+        # initial form render
+        return render(request, 'students/groups_add.html',
+        {'students': Student.objects.all().order_by('last_name')})
+
+
+    return render(request, 'students/groups_add.html',
+        {'students': Group.objects.all().order_by('last_name')})
 
 
 def groups_edit(request, gid):
@@ -61,16 +114,17 @@ class GroupDeleteView(DeleteView):
     def dispatch(self, *args, **kwargs):
         try:
             return super(GroupDeleteView, self).dispatch(*args, **kwargs)
+        # can we use ProtectedError without import ?
         except ProtectedError:
             #TODO: use massages
-            #messages.add_message(request,messages.INFO, 'Неможливо видалити группу: вона містить студентів!')
+            #messages.add_message(request,messages.INFO, 'Неможливо видалити групу: вона містить студентів!')
             url = self.get_success_url()
-            return HttpResponseRedirect(url+u'?status_message=Неможливо видалити группу: вона містить студентів!')
+            return HttpResponseRedirect(url+u'?status_message=Неможливо видалити групу: вона містить студентів!')
 
 
     def get_success_url(self):
         #TODO: message about success deletion 
-        return reverse('home')
+        return reverse('groups')
 
     def post(self, request, *args, **kwargs):
         if "cancel_button" in request.POST:
